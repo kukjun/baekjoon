c600806fdd5fd2a231a9ec57039838c9492a78a1
Kukjun Lee <20171655.info@edu.hanbat.ac.kr>
2022-01-12 22:33:28 +0900

    <feature> : <Changed Problem9461 from progress to complete>
    * Upload Problem9461

---
+++ baekjoon/src/progress/Problem14713.java
@@ -0,0 +1,25 @@
+package progress;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Problem14713 {
+  public static void main(String[] args) {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st;
+
+    try {
+      int T = Integer.parseInt(br.readLine());
+      Queue<String>[] queues = new Queue[T];
+      for (int i = 0; i < T; i++) {
+
+      }
+
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+
+  }
+}
